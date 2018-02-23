5b015a4624905cfdafc237ea1dfac512008973af
hiracky16 <h.piiice16@gmail.com>
2018-02-23 23:19:35 +0900

    seed の編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+puts "Delete data"
+Poem.delete_all
+puts "Finish Deleting"
+
+puts "Start Insert Poem"
+CSV.foreach('db/hyakunin.csv') do |row|
+    Poem.create do |u|
+        u.id = row[0]
+        u.kaminoku = row[1]
+        u.shimonoku = row[2]
+        u.kami_furigana = row[3]
+        u.shimo_furigana = row[4]
+        u.author = row[5]
+    end
+end
+puts "End Insert Poem"
