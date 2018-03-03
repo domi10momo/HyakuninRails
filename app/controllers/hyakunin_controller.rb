577de14f12f588120326147418a63ee3b934c0d6
hiracky16 <h.piiice16@gmail.com>
2018-03-04 00:41:24 +0900

    読み札の取得と札の整列

--- app/controllers/hyakunin_controller.rb
+++ app/controllers/hyakunin_controller.rb
@@ -4,6 +4,13 @@ class HyakuninController < ApplicationController
 
   def game
     @poems = Poem.all
-    @use_poems = @poems.sample(25)
+    @read_poems = @poems.sample(50)
+    @use_poems = @read_poems.sample(25)
+    getReadPoem()
+  end
+
+  def getReadPoem
+    @read_poem = @read_poems.sample(1).first
+    @read_poems.delete(@read_poem)
   end
 end
